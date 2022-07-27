b5fb81cd15b18ccbd1a9d29ff0a3b65d41a49323
OakleyYoung <108936607+OakleyYoung@users.noreply.github.com>
2022-07-27 10:07:06 +0100

    V 1.1
    NEW MODULES

--- code/ULMO.f90
+++ code/ULMO.f90
@@ -8,22 +8,22 @@ implicit none
 !*******************
 !!! CODE TESTING !!!
 !*******************
-!real*8,  dimension(N_LATS,N_LONS) :: mass_flux_THETA
-!real*8,  dimension(N_LATS,N_LONS) :: mass_flux_PHI
-!real*8,  dimension(N_LATS,N_LONS) :: sv_mass_flux_THETA
-!real*8,  dimension(N_LATS,N_LONS) :: sv_mass_flux_PHI
+real*8,  dimension(N_LATS,N_LONS) :: mass_flux_THETA
+real*8,  dimension(N_LATS,N_LONS) :: mass_flux_PHI
+real*8,  dimension(N_LATS,N_LONS) :: sv_mass_flux_THETA
+real*8,  dimension(N_LATS,N_LONS) :: sv_mass_flux_PHI
 !
-!mass_flux_THETA = calculate_mass_flux_THETA()
-!mass_flux_PHI = calculate_mass_flux_PHI()
-!sv_mass_flux_THETA =calculate_flow_sv_THETA()
-!sv_mass_flux_PHI =calculate_flow_sv_PHI()
-!
+mass_flux_THETA = calculate_mass_flux_THETA()
+mass_flux_PHI = calculate_mass_flux_PHI()
+sv_mass_flux_THETA =calculate_flow_sv_THETA()
+sv_mass_flux_PHI =calculate_flow_sv_PHI()
+
 !
 !!! Writing mass flux outputs to files !!
-!call write_file('sv_mass_flux_Phi.dat',sv_mass_flux_Phi,N_LATS,N_LONS)
-!call write_file('mass_flux_PHI.dat',mass_flux_PHI,N_LATS,N_LONS)
-!call write_file('sv_mass_flux_Theta.dat',sv_mass_flux_THETA,N_LATS,N_LONS)
-!call write_file('mass_flux_Theta.dat',mass_flux_THETA,N_LATS,N_LONS)
+call write_file('sv_mass_flux_Phi.dat',sv_mass_flux_Phi,N_LATS,N_LONS)
+call write_file('mass_flux_PHI.dat',mass_flux_PHI,N_LATS,N_LONS)
+call write_file('sv_mass_flux_Theta.dat',sv_mass_flux_THETA,N_LATS,N_LONS)
+call write_file('mass_flux_Theta.dat',mass_flux_THETA,N_LATS,N_LONS)
 
 
 end program ULMO
