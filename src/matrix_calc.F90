c0eb63a864de565bf2674c61cac54b649c75dbe0
Oakley Young <ocfy201@um-serve>
2022-08-16 17:02:52 +0100

    memory checking

--- src/matrix_calc.F90
+++ src/matrix_calc.F90
@@ -8,11 +8,9 @@ use READ_DATA
 use heat_fluxes
 use HEIGHT_OF_SLAB
 use fgsl
-use dA_da
-use div_m
 use, intrinsic :: iso_fortran_env
 implicit none
-public :: calculate_matrix , calculate_vector_f_values , calculate_new_T
+public :: calculate_matrix , calculate_vector_f , calculate_new_T
 private
 
 contains
@@ -43,192 +41,49 @@ end function calculate_new_lon
 ! Calculates and sets the matrix elements for a 2 layer ocean with 144 longitude points and 90 latitude points.
 ! version determines whether the matrix is calculated for a no horizontal transport model, diffusion only model or the full Ekman model.
 !**************************************************************************************************************************************
-subroutine calculate_matrix(land_mask,A)!,M,version)
+subroutine calculate_matrix(A)
   ! NO TRANSPORT !
-  ! version: 0 = no transport, 1= Diffusion, 2 = Diffusion ekman
-  !integer(int64), intent(in) :: version
-  integer(int64), dimension(:,:), intent(in) :: land_mask
-  !real(real64),dimension(:,:,:), intent(in) :: M
-  !real(real64) :: thickness,s_ekman,theta,dM_theta_dtheta,dM_phi_dphi,M_theta,M_phi,div_M,d_theta,d_phi,s_dfsn
-  !real(int64), dimension(:,:),allocatable :: lats_data,lats
-  integer(int64):: lat,lon,height!,lon_j,lat_j
-  integer(fgsl_size_t) ::  i !,j
-  integer(fgsl_int) :: status
-  real(fgsl_double) :: Aii,Aij
   type(fgsl_spmatrix),intent(out) :: A
+  integer(fgsl_size_t) ::  i
+  integer(int64):: lat,lon,height
+  integer(fgsl_int) :: status
+  real(fgsl_double) :: Aii
+  !integer, dimension(25920) :: mat_indexes
 
   ! matrix size = 144x90x2+90x144+144 = 39024 (height*N_LATS*N_LONS+lat*N_LONS+lon)
   A = fgsl_spmatrix_alloc(25920_fgsl_size_t, 25920_fgsl_size_t)
 
-  !allocate(lats_data(N_LATS,2),lats(N_LATS,1))
-
-  !d_theta = deg_to_rad(DELTA_LAT)
-  !d_phi = deg_to_rad(DELTA_LON)
-  !s_dfsn = DELTA_T*D/(R_PLANET**2)
 
   do height = 0,N_DEPTHS-1
-    !thickness = h_slab(height+1)
-    !s_ekman = DELTA_T/(RHO_WATER*thickness*R_PLANET)
     do lat = 0,N_LATS-1
-        !lats_data = read_file_real(LATS_FILE,N_LATS,2_int64) ! This can be converted to a grid input
-        !lats(:,1)= lats_data(:,2) ! This can be converted to a grid input
-        !theta = deg_to_rad(lats(lat+1,1))
         do lon = 0,N_LONS-1
+
+
             i = calculate_matrix_index(lon,lat,height)
-            ! check if land !
-            if(land_mask(lat+1,lon+1)==1) then
-                Aii = 1
-                status = fgsl_spmatrix_set(A,i,i,Aii)
-                cycle ! ask about this
-            end if
+            !print*,i
 
-            !M_theta = M(1,lat+1,lon+1)
-            !M_phi = M(2,lat+1,lon+1)
-            !call dA_d_theta(M(1,:,:),lat+1,lon+1,dM_theta_dtheta) ! +1 as these are Fortran arrays
-            !call dA_d_phi(M(2,:,:),lat+1,lon+1,dM_phi_dphi)
-            !call calculate_div_M(lat+1,lon+1,M(2,:,:),M(1,:,:),div_M)
-            Aij = 0.0
-            ! central point !
+            !mat_indexes(i+1) = i
+
             Aii = 1 ! no transport makes all diagonal components 1
-
-!            if(version /= 0) then
-!                Aii = Aii+2.*s_dfsn*(1./(d_theta**2)+1./((cos(theta)*d_phi)**2))
-!                if(version == 2) then
-!                    if(height == 0) then
-!                        Aii = Aii + s_ekman*(dM_theta_dtheta-M_theta*tan(theta)+1./cos(theta)*dM_phi_dphi)
-!                        if(div_M < 0.0) then
-!                            Aii = Aii +(-DELTA_T/(RHO_WATER*thickness)*div_M)
-!                        end if
-!                    elseif(height == 1) then
-!                        Aii = Aii + (-s_ekman)*(dM_theta_dtheta-M_theta*tan(theta)+1./cos(theta)*dM_phi_dphi)
-!                        if(div_M > 0.0) then
-!                            Aii = Aii+(+DELTA_T/(RHO_WATER*thickness)*div_M)
-!                        end if
-!                    end if
-!                end if
-!            end if
-
             status = fgsl_spmatrix_set(A, i, i, Aii) ! build the sparse matrix
+            !print*,'A matrix','(',i,2,')','=' ,fgsl_spmatrix_get(A,i,2_fgsl_size_t)
 
-            ! Neighbouring points !
-!            if(version /= 0) then
-!                ! neighbouring latitude - below !
-!                if(lat == 0) then
-!                    lon_j = calculate_new_lon(lon)
-!                    lat_j = lat
-!                    j = calculate_matrix_index(lon_j,lat_j,height)
-!                else
-!                    lon_j = lon
-!                    lat_j = lat
-!                    j = calculate_matrix_index(lon_j,lat_j,height)
-!                end if
-!
-!                Aij = -s_dfsn*(1./(d_theta)**2+tan(theta)/(2*d_theta))
-!
-!                if(version == 2) then
-!                    if(height == 0) then
-!                        Aij = Aij + s_ekman*(-M_theta/(2.*d_theta))
-!                    else
-!                        Aij = Aij + (-s_ekman)*(-M_theta/(2.*d_theta))
-!                    end if
-!                end if
-!
-!                if(land_mask(lat_j+1,lon_j+1) == 1) then
-!                    Aii = Aii + Aij
-!                    Aij = 0.
-!                end if
-!
-!                status = fgsl_spmatrix_set(A,i,j,Aij)
-!
-!                ! neighbouring longitude - left !
-!
-!                if(lon == 0) then
-!                    lon_j = n_lons-1
-!                    lat_j = lat
-!                    j = calculate_matrix_index(lon_j,lat_j,height)
-!                else
-!                    lon_j = lon-1
-!                    lat_j = lat
-!                    j = calculate_matrix_index(lon_j,lat_j,height)
-!                end if
-!
-!                Aij = -s_dfsn*1./(cos(theta)*d_phi)**2
-!
-!                if(version == 2) then
-!                    if(height == 0) then
-!                        Aij = Aij + s_ekman*(-M_phi/(2.*d_phi*cos(theta)))
-!                    else
-!                        Aij = Aij - s_ekman*(-M_phi/(2.*d_phi*cos(theta)))
-!                    end if
-!                end if
-!
-!                if(land_mask(lat_j+1,lon_j+1) == 1) then
-!                    Aii = Aii + Aij
-!                    Aij = 0.
-!                end if
-!
-!                status = fgsl_spmatrix_set(A,i,j,Aij)
-!
-!                ! neighbouring longitude - right !
-!
-!                if(lon == N_LONS-1) then
-!                    lon_j = 0
-!                    lat_j = lat
-!                    j = calculate_matrix_index(lon_j,lat_j,height)
-!                else
-!                    lon_j = lon+1
-!                    lat_j = lat
-!                    j = calculate_matrix_index(lon_j,lat_j,height)
-!                end if
-!
-!                Aij = -s_dfsn*1./(cos(theta)*d_phi)**2
-!
-!                if(version == 2) then
-!                    if(height == 0) then
-!                        Aij = Aij + s_ekman*(M_phi/(2.*d_phi*cos(theta)))
-!                    else
-!                        Aij = Aij + (-s_ekman)*(M_phi/(2.*d_phi*cos(theta)))
-!                    end if
-!                end if
-!
-!                if(land_mask(lat_j+1,lon_j+1) == 1) then
-!                    Aii = Aii + Aij
-!                    Aij = 0.
-!                end if
-!
-!                status = fgsl_spmatrix_set(A,i,j,Aij)
-!                status = fgsl_spmatrix_set(A,i,i,Aii)
-!
-!                ! Interaction between deep and surface layer !
-!
-!                if(version == 2) then
-!                    if(height == 0 .and. div_M > 0.0) then
-!                        Aij = (-DELTA_T/(RHO_WATER*thickness)*div_M)
-!                        j = calculate_matrix_index(lon,lat,1_int64)
-!                        status = fgsl_spmatrix_set(A,i,j,Aij)
-!                    elseif(height == 1 .and. div_M < 0.0) then
-!                        Aij = (+DELTA_T/(RHO_WATER*thickness)*div_M)
-!                        j = calculate_matrix_index(lon,lat,0_int64)
-!                        status = fgsl_spmatrix_set(A,i,j,Aij)
-!                    end if
-!                end if
-
-            !end if
         end do
     end do
   end do
-
-  !deallocate(lats_data,lats)
-  !call fgsl_spmatrix_free(A)
-
+!print*,'mat index = ',mat_indexes
+!print*,'mat index = ',size(mat_indexes)
 end subroutine calculate_matrix
+
+
+
 !**********************************************************************************************************************
 ! calculates and sets the vector b in matrix Ax=b. EMISSIVITY*SIGMA*T^4 is a blackbody emission from one of the layers,
 ! to ensure an energy exchange between the layers. 1.0/(RHO_WATER*C_V*H_S) prefactor converts a flux in W/m2 to K/s
 !**********************************************************************************************************************
-subroutine calculate_vector_f_values(T,f_f,land_mask)
+subroutine calculate_vector_f(T,f_f)!,B)
     real(real64),dimension(:,:,:),intent(in) :: T
-    integer(int64), dimension(:,:), intent(in) :: land_mask
+    !integer(int64), dimension(N_LATS,N_LONS), intent(in) :: land_mask
     real(real64), dimension(:,:),allocatable :: F_a,F_c
     real(real64), dimension(:),allocatable :: vec_data
     real(real64) :: depth,b_hij
@@ -236,56 +91,78 @@ subroutine calculate_vector_f_values(T,f_f,land_mask)
     ! fgsl !
     integer(fgsl_size_t), parameter :: n = 25920
     real(fgsl_double),dimension(:),intent(out) :: f_f
-    integer(fgsl_size_t) :: vec_index
+    integer(fgsl_size_t) :: vec_index !,size_vec
     real(fgsl_double) :: vec_val
-
+    !type(fgsl_vector),intent(out) :: f
 
     allocate(F_c(N_LATS,N_LONS),F_a(N_LATS,N_LONS),vec_data(n))
 
     SURFACE = 1 ! position in 3D temperatures array (1,:,:)
     DEEP = 2    ! position in 3D temperatures array (2,:,:)
 
+    !b_hij = 0.0
+
     call calculate_F_a(F_a)
     call calculate_F_c(T,F_c)
 
+    !testing (works)!
+!    h = 2
+!    depth=h_slab(h)
+!    print*,'depth deep',depth
+!    b_hij = (DELTA_T/(RHO_WATER*C_V*depth))*(-F_c(1,1)+T(2,1,1))
+!    print*,'deep = ',b_hij
+!    h=1
+!    depth= h_slab(h)
+!    print*,'depth deep',depth
+!    b_hij = (DELTA_T/(RHO_WATER*C_V*depth))*(F_c(1,1)+F_a(1,1)-EMISSIVITY*SIGMA*(T(SURFACE,1,1))**4)*T(SURFACE,1,1)
+!    print*,'surf=',b_hij
     do h = 1,N_DEPTHS
         do i = 1, N_LATS
             do j= 1,N_LONS
-                if(land_mask(i,j)==1) then
-                    b_hij = T(h,i,j)
+
+                depth = h_slab(h)
+                if (h==1) then
+                    b_hij = DELTA_T/(RHO_WATER*C_V*depth)*(F_c(i,j)+F_a(i,j)-EMISSIVITY*SIGMA*((T(h,i,j))**4))+T(h,i,j)
                 else
-                    depth = h_slab(h)
-                    if (h==1) then
-                        b_hij = DELTA_T/(RHO_WATER*C_V*depth)*(F_c(i,j)+F_a(i,j)-EMISSIVITY*SIGMA*((T(h,i,j))**4))+T(h,i,j)
-                    else
-                        b_hij = DELTA_T/(RHO_WATER*C_V*depth)*(-F_c(i,j))+T(h,i,j) !! double check this with Jake !!
+                    b_hij = DELTA_T/(RHO_WATER*C_V*depth)*(-F_c(i,j))+T(h,i,j) !! double check this with Jake !!
                 end if
+                !print*,b_hij
+
                 vec_index = (h-1)*N_LATS*N_LONS+(i-1)*N_LONS+(j-1) ! fgsl index
                 vec_data(vec_index+1) = b_hij ! needs fortran index
+                !print*, vec_index
                 vec_val = b_hij ! making sure the type is fgsl_size_t
+                !print*, vec_val
                 f_f(vec_index+1) = vec_val
 
-                end if
+
+
             end do
         end do
     end do
 
-    deallocate(F_a,F_c,vec_data)
 
-end subroutine calculate_vector_f_values
+!print*, 'vec = ',v
+!f = fgsl_vector_init(f_f)
+    deallocate(F_a,F_c,vec_data)
+!size_vec =  fgsl_vector_get_size(B)
+!print*, 'size=', size_vec
+
+
+end subroutine calculate_vector_f
 !!**************************************************************
 !! Subroutine that uses FGSL sparse linear algebra to solve Ax=b
 !!**************************************************************
-subroutine calculate_new_T(T,land_mask)!,M,version)
+subroutine calculate_new_T(T)
 
-    !integer(int64),intent(in) :: version
-    integer(int64), dimension(:,:), intent(in) :: land_mask
-    real(real64), dimension(:,:,:), intent(inout) :: T!,M
+    real(real64), dimension(:,:,:), intent(inout) :: T
+    !integer(int64), dimension(N_LATS,N_LONS), intent(in) :: land_mask
     integer(int64) :: h,i,j
     ! fgsl !
     type(fgsl_spmatrix):: A,C
     real(fgsl_double):: residual
     integer(fgsl_size_t),parameter:: n = 25920
+    !integer(fgsl_size_t) :: size_vec
     integer(fgsl_int) :: status
     integer(fgsl_size_t) :: iter = 0
     real(fgsl_double), parameter :: tol = 1.0e-6
@@ -299,7 +176,6 @@ subroutine calculate_new_T(T,land_mask)!,M,version)
 
     !A = fgsl_spmatrix_alloc(n,n)
     allocate(u_f(n),f_f(n))
-
 
 
 
@@ -317,7 +193,7 @@ subroutine calculate_new_T(T,land_mask)!,M,version)
 
     f = fgsl_vector_init(f_f)
 
-    call calculate_vector_f_values(T,f_f,land_mask)
+    call calculate_vector_f(T,f_f)
 
 
 
@@ -327,7 +203,7 @@ subroutine calculate_new_T(T,land_mask)!,M,version)
     !print*, 'size of f=', size_vec
 
 
-    call calculate_matrix(land_mask,A)
+    call calculate_matrix(A)
     ! Matrix check !
 !    do i = 0_fgsl_size_t,25919_fgsl_size_t
 !        print*, fgsl_spmatrix_get(A,i,i) ! These should all be 1
--- src/ulmo.F90
+++ src/ulmo.F90
@@ -1,67 +1,59 @@
-
 program ULMO
     use MASS_FLUX
     use READ_DATA
     use NAMELIST
     use MATRIX_CALC
     use fgsl
+    !use time_stepper
     use process_output_data
     use, intrinsic :: iso_fortran_env
 implicit none
 !*********************
 !! ULMO MAIN SCRIPT !!
 !*********************
+!!! Matrix calculation testing !!! ****WORKS****
+!! Mass flux testing !!
+!real(real64), dimension(:,:),allocatable :: u_wind,v_wind,sv_flow_phi,sv_flow_theta
+!integer(int64), dimension(:,:), allocatable :: land_mask
+!
+!allocate(u_wind(N_LATS,N_LONS),v_wind(N_LATS,N_LONS),land_mask(N_LATS,N_LONS),sv_flow_phi(N_LATS,N_LONS)&
+!,sv_flow_theta(N_LATS,N_LONS))
+!
+!u_wind = read_file_real(U_WIND_DATA,N_LATS,N_LONS)
+!v_wind = read_file_real(V_WIND_DATA,N_LATS,N_LONS)
+!land_mask = read_file_int(LAND_MASK_DATA,N_LATS,N_LONS)
+!
+!call calculate_flow_sv_PHI(u_wind,v_wind,land_mask,sv_flow_phi)
+!call calculate_flow_sv_THETA(u_wind,v_wind,land_mask,sv_flow_theta)
+!
+!call write_file('output_data/sv_flow_Phi.dat',sv_flow_phi,N_LATS,N_LONS)
+!call write_file('output_data/sv_flow_Theta.dat',sv_flow_theta,N_LATS,N_LONS)
+!
+!deallocate(u_wind,v_wind,land_mask,sv_flow_phi,sv_flow_theta)
 
-!! Version: 0 = no transport, 1 = diffusion only, 2 = Ekman and diffusion !!
-
-real(real64), dimension(:,:),allocatable :: u_wind,v_wind,mass_flux_phi,mass_flux_theta,sv_flow_phi,sv_flow_theta
-integer(int64), dimension(:,:), allocatable :: land_mask
-real(real64),dimension(:,:,:),allocatable :: T, M
+real(real64),dimension(:,:,:),allocatable :: T
 real(real64), dimension(:,:),allocatable :: T_surf,T_deep,T_surf_out
 
 integer(int64) :: n_times ! time stepper testing
 !real(fgsl_double),dimension(:),target,allocatable :: f_f ! vector testing
 !type(fgsl_vector) :: f ! vector testing
 !integer(fgsl_size_t),parameter:: n = 25920 ! vector testing
-!real(real64) :: time, days !,avg_T_surf_init,avg_T_surf_new ! timestepper testing
+real(real64) :: time, days !,avg_T_surf_init,avg_T_surf_new ! timestepper testing
 integer(int64) :: ti ! time stepper testing
 
-n_times = 2
+n_times = 20
 
-allocate(T(2,N_LATS,N_LONS),M(2,N_LATS,N_LONS),T_surf(N_LATS,N_LONS),T_deep(N_LATS,N_LONS),T_surf_out(n_times,1) &
-        ,u_wind(N_LATS,N_LONS),v_wind(N_LATS,N_LONS),land_mask(N_LATS,N_LONS),mass_flux_phi(N_LATS,N_LONS)       &
-        , mass_flux_theta(N_LATS,N_LONS),sv_flow_phi(N_LATS,N_LONS),sv_flow_theta(N_LATS,N_LONS))
+allocate(T(2,N_LATS,N_LONS),T_surf(N_LATS,N_LONS),T_deep(N_LATS,N_LONS),T_surf_out(n_times,1))
 
 !allocate(f_f(n)) ! vector testing
-
-land_mask = read_file_int(LAND_MASK_DATA,N_LATS,N_LONS)
 
 T_surf= read_file_real(INITIAL_SURFACE_TEMP_DATA,N_LATS,N_LONS)
 T_deep= read_file_real(INITIAL_DEEP_TEMP_DATA,N_LATS,N_LONS)
 
-! Surface = 1 Deep = 2
 T(1,:,:) = T_surf
 T(2,:,:) = T_deep
 
-u_wind = read_file_real(U_WIND_DATA,N_LATS,N_LONS)
-v_wind = read_file_real(V_WIND_DATA,N_LATS,N_LONS)
-call calculate_mass_flux_PHI(u_wind,v_wind,land_mask,mass_flux_phi)
-call calculate_mass_flux_THETA(u_wind,v_wind,land_mask,mass_flux_theta)
-
-! Theta = 1 Phi = 2
-M(1,:,:) = mass_flux_theta
-M(2,:,:) = mass_flux_phi
-
-! Initial mass flux tests !
-call calculate_flow_sv_PHI(u_wind,v_wind,land_mask,sv_flow_phi)
-call calculate_flow_sv_THETA(u_wind,v_wind,land_mask,sv_flow_theta)
-call write_file('output_data/sv_flow_Phi.dat',sv_flow_phi,N_LATS,N_LONS)
-call write_file('output_data/sv_flow_Theta.dat',sv_flow_theta,N_LATS,N_LONS)
-
-
-
-
-!! Calculate_matrix !! ****WORKS**** For version = 0
+!! Calculate_matrix !! ****WORKS****
 !lat = 1
 !lon = 1
 !height =1
@@ -87,11 +79,11 @@ call write_file('output_data/sv_flow_Theta.dat',sv_flow_theta,N_LATS,N_LONS)
 !time = (ti+1)*DELTA_T
 
 do ti = 1,n_times
-    call calculate_new_T(T,land_mask)
+    call calculate_new_T(T)
     T_surf_out(ti,1) = sum(T(1,:,:))/(144*90)
 end do
-!print*, T_surf_out
-call write_file('output_data/ProCb/T_surf.dat',T_surf_out,n_times,1_int64)
+print*, T_surf_out
+!call write_file('output_data/ProCb/T_surf.dat',T_surf_out,n_times,1_int64)
 
 
 !! t_stepper !!
@@ -118,14 +110,9 @@ call write_file('output_data/ProCb/T_surf.dat',T_surf_out,n_times,1_int64)
 !end do
 
 
-deallocate(T,M,T_surf,T_deep,T_surf_out,u_wind,v_wind,mass_flux_phi,mass_flux_theta,land_mask &
-            , sv_flow_phi,sv_flow_theta)
+deallocate(T,T_surf,T_deep,T_surf_out)
 
 
 
 
 end program ULMO
-
-
-
-
