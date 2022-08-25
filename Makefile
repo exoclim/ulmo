804f85b036cc7c2d51c4c7e2366c8f4889745d9a
Oakley Young <ocfy201@um-serve>
2022-08-25 16:37:20 +0100

    Forward Euler

--- Makefile
+++ Makefile
@@ -6,16 +6,9 @@ FFLAGS=-O3 -Wall -Wextra -std=f2008 -fcheck=all -g -fall-intrinsics
 INCLUDE=-I${PREFIX}/include/fgsl -I${PREFIX}/include
 LIBS=-L${PREFIX}/lib -lfgsl -lgsl -lgslcblas -lm -lcblas -lm
 
-<<<<<<< HEAD
 EXE=ulmo.out
-#Uncomment 1st SRC if want fgsl solver
-#SRC=./src/namelist.F90 ./src/read_data.F90 ./src/degree_to_radian.F90 ./src/height_of_slab.F90 ./src/mass_fluxes.F90 ./src/heat_fluxes.F90 ./src/dA_da.F90 ./src/div_m.F90 ./src/matrix_calc.F90 ./sr#c/memory_usage.F90 ./src/process_output_data.F90 ./src/ulmo_fgsl.F90
-SRC=./src/namelist.F90 ./src/read_data.F90 ./src/degree_to_radian.F90 ./src/height_of_slab.F90 ./src/mass_fluxes.F90 ./src/heat_fluxes.F90 ./src/dA_da.F90 ./src/div_m.F90 ./src/process_output_data.F90 ./src/new_temp_calc_fe.F90 ./src/ulmo_fe.F90
-
-=======
-EXE=main.out
-SRC=./src/constants.F90 ./src/read_data.F90 ./src/degree_to_radian.F90 ./src/height_of_slab.F90 ./src/heat_fluxes.F90 ./src/matrix_calc.F90 ./src/process_output_data.F90 ./src/main.F90
->>>>>>> 54079431224abd92ef840aca5829df8ea309df91
+#SRC=./src/constants.F90 ./src/read_data.F90 ./src/degree_to_radian.F90 ./src/height_of_slab.F90 ./src/mass_fluxes.F90 ./src/heat_fluxes.F90 ./src/dA_da.F90 ./src/div_m.F90 ./src/matrix_calc.F90 ./src/memory_usage.F90 ./src/process_output_data.F90 ./src/ulmo_fgsl.F90
+SRC=./src/constants.F90 ./src/read_data.F90 ./src/degree_to_radian.F90 ./src/height_of_slab.F90 ./src/mass_fluxes.F90 ./src/heat_fluxes.F90 ./src/dA_da.F90 ./src/div_m.F90 ./src/process_output_data.F90 ./src/new_temp_calc_fe.F90 ./src/ulmo_fe.F90
 OBJ=${SRC:/src/.f90=.o} #substitute .f90 with .o
 
 help:
--- src/ulmo_fgsl.F90
+++ src/ulmo_fgsl.F90
@@ -1,18 +1,28 @@
-program main
+program ULMO
 
     !**This version of Ulmo uses the fgsl linear algebra solver to solve differential equations**!
+
+    use MASS_FLUX, only: calculate_surface_stress_PHI,calculate_surface_stress_THETA,calculate_flow_sv_THETA &
+    ,calculate_mass_flux_THETA,calculate_mass_flux_PHI,calculate_flow_sv_PHI
     use READ_DATA, only: read_file_real,read_file_int,write_file
     use Constants
     use heat_fluxes, only: calc_Q_flux,calculate_F_a,calculate_F_c
     use MATRIX_CALC, only: calculate_matrix_index,calculate_matrix,calculate_vector_f_values
     use fgsl
+    use memory_usage
     use, intrinsic :: iso_fortran_env
+    !**TESTING**!
     use process_output_data
 implicit none
 
 !*********************
 !! ULMO MAIN SCRIPT !!
 !*********************
+
+!**Mass flux variables**!
+real(real64), dimension(:,:),allocatable :: u_wind,v_wind,tau_PHI,tau_THETA,mass_flux_THETA &
+                                            ,mass_flux_PHI,sv_flow_PHI,sv_flow_THETA
+real(real64), dimension(:,:,:), allocatable :: M
 
 !**Heat flux variables**!
 real(real64), dimension(:,:), allocatable :: F_net_sw_down, F_lw_down,F_latent_up,F_sensible_up
@@ -27,13 +37,14 @@ real(real64), dimension(:,:),allocatable :: lats,lats_data
 !**Matrix/Vector fgsl variables**!
 type(fgsl_spmatrix) :: A,C
 integer(fgsl_size_t), parameter :: n = 25920
+!real(fgsl_double),dimension(:),allocatable :: f_f,u_f ! could make these a target
 real(fgsl_double),dimension(1:n),target :: f_f, u_f ! target is used in git hub example
 type(fgsl_vector) :: f,u
 integer(int64) :: h,i,j
 real(fgsl_double):: residual
 integer(fgsl_int) :: status
 integer(fgsl_size_t) :: iter = 0
-real(fgsl_double), parameter :: tol = 1.0e-6
+!real(fgsl_double), parameter :: tol = 1.0e-6
 type(fgsl_splinalg_itersolve_type), parameter :: S = fgsl_splinalg_itersolve_gmres
 type(fgsl_splinalg_itersolve) :: work
 
@@ -41,19 +52,67 @@ type(fgsl_splinalg_itersolve) :: work
 integer(int64) :: n_step,n_times
 real(real64) :: days,time
 
+!**Memory usage varaiables**!
+integer :: valueRSS
+real(real64),dimension(:), allocatable :: Rss_data
 
+!**CPU Time varaiables**!
+real(real64) :: start, finish
+real(real64),dimension(:,:),allocatable:: cpu_data
 
-!**Land Mask and coords**!
+!**SYSTEM ANALYSIS**!
+call cpu_time(start)
+allocate(Rss_data(2005))
+call system_mem_usage(valueRSS)
+Rss_data(1) = valueRSS
+write (*,"(a18,i5)") 'valueRSS start =',valueRSS
+
+!**Land Mask allocation and load**!
 allocate(land_mask(N_LATS,N_LONS))
 land_mask = read_file_int(LAND_MASK_DATA,N_LATS,N_LONS)
 
+!**Calculating surface stresses from u and v wind**!
+allocate(u_wind(N_LATS,N_LONS),v_wind(N_LATS,N_LONS),tau_PHI(N_LATS,N_LONS),tau_THETA(N_LATS,N_LONS))
+
+u_wind = read_file_real(U_WIND_DATA,N_LATS,N_LONS)
+v_wind = read_file_real(V_WIND_DATA,N_LATS,N_LONS)
+
+call calculate_surface_stress_PHI(u_wind,tau_PHI)
+call calculate_surface_stress_THETA(v_wind,tau_THETA)
+
+deallocate(u_wind,v_wind)
+
+!**Calculating mass fluxes from surface stresses**!
 allocate(lats_data(N_LATS,2),lats(N_LATS,1))
+
+allocate(mass_flux_THETA(N_LATS,N_LONS),mass_flux_PHI(N_LATS,N_LONS))
 
 lats_data = read_file_real(LATS_FILE,N_LATS,2_int64)
 lats(:,1)= lats_data(:,2) ! Second column in lats data file includes the latitude points
 
 deallocate(lats_data)
 
+call calculate_mass_flux_THETA(lats,tau_PHI,tau_THETA,land_mask,mass_flux_THETA)
+call calculate_mass_flux_PHI(lats,tau_PHI,tau_THETA,land_mask,mass_flux_PHI)
+
+deallocate(tau_PHI,tau_THETA)
+
+!**Calculating mass flux in Sverdrups**!!
+allocate(sv_flow_PHI(N_LATS,N_LONS),sv_flow_THETA(N_LATS,N_LONS))
+
+call calculate_flow_sv_PHI(mass_flux_PHI,sv_flow_PHI)
+call calculate_flow_sv_THETA(lats,mass_flux_THETA,sv_flow_THETA)
+
+!**Writing mass flux initial output to files**!
+!call write_file('output_data/sv_flow_PHI_init.dat',sv_flow_PHI,N_LATS,N_LONS)
+!call write_file('output_data/sv_flow_Theta_init.dat',sv_flow_THETA,N_LATS,N_LONS)
+
+allocate(M(2,N_LATS,N_LONS))
+
+M(1,:,:) = mass_flux_PHI(:,:) !PHI = 1
+M(2,:,:) = mass_flux_THETA(:,:) !THETA = 2
+
+deallocate(mass_flux_PHI,mass_flux_THETA,lats,sv_flow_PHI,sv_flow_THETA)
 
 !**Heat flux calculations**!
 allocate(F_net_sw_down(N_LATS,N_LONS),F_lw_down(N_LATS,N_LONS),F_latent_up(N_LATS,N_LONS),F_sensible_up(N_LATS,N_LONS))
@@ -73,6 +132,10 @@ T(2,:,:) = T_deep_init(:,:)
 
 deallocate(T_surf_init,T_deep_init)
 
+!**SYSTEM ANALYSIS**!
+call system_mem_usage(valueRSS)
+write (*,"(a49,i5)") 'valueRSS before allocating vectors and matrix =',valueRSS
+Rss_data(2) = valueRSS
 
 !**Calculating F_a**!
 call calculate_F_a(F_net_sw_down,F_lw_down,F_latent_up,F_sensible_up,F_a) ! Only depends on um inputs
@@ -90,29 +153,51 @@ call calculate_matrix(land_mask,A) ! allocated using subroutine
 C = fgsl_spmatrix_compcol(A) ! compressed column format
 
 
+!**SYSTEM ANALYSIS**!
+call system_mem_usage(valueRSS)
+write (*,"(a49,i5)") 'valueRSS before time stepping =',valueRSS
+Rss_data(3) = valueRSS
+call cpu_time(finish)
+print '("CPU time before time stepping = ",f6.3," seconds.")',finish-start
+allocate(cpu_data(n_times,1_int64))
 
-!**CALCULATING NEW TEMP USING FGSL**!
-n_times = 200 !number of time steps 20 steps = 10 days
+
+!**CALCULATING NEW TEMP**!
+n_times = 2000 !number of time steps 20 steps = 10 days
 do n_step = 1,n_times
+
+    !**SYSTEM ANALYSIS**!
+    call cpu_time(start)
+    call system_mem_usage(valueRSS)
+    write(*,"(a40,i5,a1,i6)") 'valueRSS before solving F_c etc',n_step,'=',valueRSS
+    Rss_data(n_step+2) = valueRSS
+
 
     call calculate_F_c(T,F_c)
     !call calc_Q_flux(T,upward_Q_flux,F_net_sw_down,F_lw_down,F_latent_up,F_sensible_up)
     call calculate_vector_f_values(land_mask,T,f_f,F_a,F_c)
 
-    work =  fgsl_splinalg_itersolve_alloc(S,n,0_fgsl_size_t)
+    !**SYSTEM ANALYSIS**!
+    call system_mem_usage(valueRSS)
+    write(*,"(a40,i5,a1,i6)") 'valueRSS before solving AU=f at n_step',n_step,'=',valueRSS
+    Rss_data(n_step+3) = valueRSS
+
     !**Solving eqaution Au = f using fgsl**!
+
+    work =  fgsl_splinalg_itersolve_alloc(S,n,0_fgsl_size_t)
+
     do
         status = fgsl_splinalg_itersolve_iterate(C, f, tol, u, work)
 
         ! print out residual norm ||A*x-b||
         residual = fgsl_splinalg_itersolve_normr(work)
-        write(output_unit, '(A,I2,A,G15.6)') 'iter ', iter, ' residual = ', residual
+        !write(output_unit, '(A,I2,A,G15.6)') 'iter ', iter, ' residual = ', residual
 
-        if (status == FGSL_SUCCESS) then
-            !write(output_unit, '(A)') 'Converged'
-        endif
+    !    if (status == FGSL_SUCCESS) then
+    !        !write(output_unit, '(A)') 'Converged'
+    !    endif
         iter = iter + 1
-        if (status /= FGSL_CONTINUE .or. iter >= 10) exit
+        if (status /= FGSL_CONTINUE .or. iter >= MAX_ITER) exit
     end do
 
     !**output solution**!
@@ -125,7 +210,13 @@ do n_step = 1,n_times
         end do
     end do
 
-    call fgsl_splinalg_itersolve_free(work)
+
+    call fgsl_splinalg_itersolve_free(work) !**CRUCIAL THAT THIS STATEMENT IS INSIDE THE LOOP**!
+
+    !**SYSTEM ANALYSIS**!
+    call cpu_time(finish)
+    print '("CPU time at step = ",I6.3,f6.3," seconds.")',n_step,finish-start
+    cpu_data(n_step,1) = finish-start
 
     !**Outputting data**!
     time = n_step*DELTA_T
@@ -142,16 +233,34 @@ do n_step = 1,n_times
 
 end do
 
+!**SYSTEM ANALYSIS**!
+call system_mem_usage(valueRSS)
+write (*,"(a31,i5)")  'valueRSS before deallocating =',valueRSS
+Rss_data(n_times+4) = valueRSS
 
 !**Deallocation of Matrices and vectors**!
-
 call fgsl_vector_free(f) ! deallocate vector
 call fgsl_spmatrix_free(A) ! deallocated matrix
 call fgsl_spmatrix_free(C)
 deallocate(F_net_sw_down,F_lw_down,F_latent_up,F_sensible_up)
-deallocate(T)
+deallocate(T,M)
 deallocate(land_mask)
 deallocate(upward_Q_flux,F_a,F_c)
 
 
-end program main
+!**SYSTEM ANALYSIS**!
+call system_mem_usage(valueRSS)
+write (*,"(a16,i5)")  'valueRSS end =',valueRSS
+Rss_data(n_times+5) = valueRSS
+call write_file('output_data/rss.dat',Rss_data,n_times+5,1_int64)
+print*, 'rss_size =', n_times+5
+call write_file('output_data/cpu_data.dat',cpu_data,n_times,1_int64)
+deallocate(Rss_data)
+deallocate(cpu_data)
+
+
+
+
+
+end program ULMO
+
